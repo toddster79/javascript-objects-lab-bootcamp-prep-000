46fc0a4c15e90123b4c0ca3a15e7d9fc72216445
Todd Carlson <todd.carlson3030@me.com>
2019-11-26 04:40:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  return object[key];
-}
+
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
