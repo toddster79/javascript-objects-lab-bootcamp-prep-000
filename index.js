deea8e08eb464cfb722bae54c7ae68c0d5690815
Todd Carlson <todd.carlson3030@me.com>
2019-11-26 03:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
